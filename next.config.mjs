6d67b041d2aa67eadbb09964787ef8e79da5a40b
Skirja <irzafinmuh11@gmail.com>
2024-11-26 12:15:53 +0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true
-    }
+    },
+    basePath: '/markdown-render',
+    assetPrefix: '/markdown-render'
 };
 
 export default nextConfig;
